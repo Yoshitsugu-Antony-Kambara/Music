fa2ff918b9557025f29573b169692a9998f3d841
Yoshitsugu Kambara <tsugu1999213@gmail.com>
2019-04-10 19:36:11 +0900

    Initial Commit

---
+++ Music/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  Music
+//
+//  Created by 神原良継 on 2019/04/10.
+//  Copyright © 2019 YoshitsuguKambara. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var audioPlayer: AVAudioPlayer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func natsuPlayButton() {
+        setAudioPlayer(soundName: "natsu", type: "mp3")
+        audioPlayer.play()
+    }
+    
+    @IBAction func amePlayButton() {
+        setAudioPlayer(soundName: "ame", type: "mp3")
+        audioPlayer.play()
+    }
+    
+    @IBAction func akiPlayButton() {
+        setAudioPlayer(soundName: "aki", type: "mp3")
+        audioPlayer.play()
+    }
+    
+    func setAudioPlayer(soundName: String, type: String) {
+        
+        let soundFilePath = Bundle.main.path(forResource: soundName, ofType: type)!
+        let fileURL = URL(fileURLWithPath: soundFilePath)
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: fileURL)
+        } catch {
+            print("音楽ファイルが読み込めませんでした")
+        }
+    }
+    
+
+}
+
